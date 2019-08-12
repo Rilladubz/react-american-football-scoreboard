6178bc2fa78d32115b2c7f7ee7e5f95687c08419
Samuel Torres <rilla@Samuels-iMac.local>
2019-08-12 16:03:44 -0400

    Break Buttons into new component file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import Buttons from "./Buttons";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -27,17 +28,7 @@ function App() {
         </div>
         <BottomRow />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={() => setHomeScore(homeScore + 6) } className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={() => setHomeScore(homeScore + 1) } className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={() => setAwayScore(awayScore + 6) } className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={() => setAwayScore(awayScore + 1) }className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
+      <Buttons />
     </div>
   );
 }
